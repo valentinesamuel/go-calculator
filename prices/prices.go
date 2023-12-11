223c278671793927eb8f09494851a2b2c5041475
valentinesamuel2580 <valentinesamuel2580@gmail.com>
2023-12-11 16:03:12 +0100

    feat: Outsourced file management to it's own module

--- prices/prices.go
+++ prices/prices.go
@@ -1,10 +1,10 @@
 package prices
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
+
+	"example.com/price_calculator/conversion"
+	"example.com/price_calculator/filemanager"
 )
 
 type TaxIncludedPriceJob struct {
@@ -14,35 +14,17 @@ type TaxIncludedPriceJob struct {
 }
 
 func (job *TaxIncludedPriceJob) LoadData() {
-	file, err := os.Open("prices.txt")
+	lines, err := filemanager.ReadLines("prices.txt")
 	if err != nil {
-		fmt.Println("And error occured")
 		fmt.Println(err)
-	}
-	scanner := bufio.NewScanner(file)
-	var lines []string
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	err = scanner.Err()
-	if err != nil {
-		fmt.Println("Reading the file content failed.")
-		fmt.Println(err)
-		file.Close()
 		return
 	}
-	prices := make([]float64, len(lines))
-	for lineIndex, line := range lines {
-		floatPrice, err := strconv.ParseFloat(line, 64)
-		if err != nil {
-			fmt.Println("Parsing the file content failed.")
-			fmt.Println(err)
-			file.Close()
-			return
-		}
-		prices[lineIndex] = floatPrice
-		job.InputPrices = prices
+	prices, err := conversion.StringsToFloats(lines)
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
+	job.InputPrices = prices
 }
 
 func (job TaxIncludedPriceJob) Process() {
