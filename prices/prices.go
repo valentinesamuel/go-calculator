1fccc4756fde2b45a3f170f2b8e6ccb5fafea0d6
valentinesamuel2580 <valentinesamuel2580@gmail.com>
2023-12-11 15:34:32 +0100

    feat: Added file reading function

--- prices/prices.go
+++ prices/prices.go
@@ -1,6 +1,11 @@
 package prices
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 type TaxIncludedPriceJob struct {
 	TaxRate           float64
@@ -8,12 +13,46 @@ type TaxIncludedPriceJob struct {
 	TaxIncludedPrices map[string]float64
 }
 
-func (job TaxIncludedPriceJob) Process() {
-	result := make(map[string]float64)
-	for _, price := range job.InputPrices {
-		result[fmt.Sprintf("%.2f", price)] = price * (1 + job.TaxRate)
+func (job *TaxIncludedPriceJob) LoadData() {
+	file, err := os.Open("prices.txt")
+	if err != nil {
+		fmt.Println("And error occured")
+		fmt.Println(err)
 	}
-	fmt.Println(result) 
+	scanner := bufio.NewScanner(file)
+	var lines []string
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	err = scanner.Err()
+	if err != nil {
+		fmt.Println("Reading the file content failed.")
+		fmt.Println(err)
+		file.Close()
+		return
+	}
+	prices := make([]float64, len(lines))
+	for lineIndex, line := range lines {
+		floatPrice, err := strconv.ParseFloat(line, 64)
+		if err != nil {
+			fmt.Println("Parsing the file content failed.")
+			fmt.Println(err)
+			file.Close()
+			return
+		}
+		prices[lineIndex] = floatPrice
+		job.InputPrices = prices
+	}
+}
+
+func (job TaxIncludedPriceJob) Process() {
+	job.LoadData()
+	result := make(map[string]string)
+	for _, price := range job.InputPrices {
+		taxIncludedPrice := price * (1 + job.TaxRate)
+		result[fmt.Sprintf("%.2f", price)] = fmt.Sprintf(" <==> %.2f |||", taxIncludedPrice)
+	}
+	fmt.Println(result)
 }
 
 func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
